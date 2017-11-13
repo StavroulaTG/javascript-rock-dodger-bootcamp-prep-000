a0cc863de6acb14075e1a632d2b7cdf39f93ea69
Stavroula Guerrero <4stacyg@optonline.net>
2017-11-13 14:22:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,14 +19,16 @@ function checkCollision(rock) {
   if (top > 360) {
     const dodgerLeftEdge = positionToInteger(DODGER.style.left)
     const dodgerRightEdge = dodgerLeftEdge + 40
+    
     const rockLeftEdge = positionToInteger(rock.style.left)
     const rockRightEdge = rockLeftEdge + 20
 
-if ( (rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge) ||
-(rockLeftEdge <= dodgerRightEdge && rockRightEdge <= dodgerRightEdge) ||
-(rockLeftEdge >= dodgerLeftEdge && rockRightEdge >= dodgerRightEdge))
+if (
+   (rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge) ||
+(rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge) ||
+(rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge))
 {
-  return true;
+  return true
 }
   }
 }
